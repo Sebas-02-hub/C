3e89cd5112604305e440a468edc90a4a4010eb66
ANDRES <sebasbeltran0205@gmail.com>
2026-04-30 12:02:20 -0500

    t

---
+++ EJERCICIO1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main(void){
+
+    int a,b;
+    printf("ingrese primer parametro:" );
+    scanf("%d",&a);
+
+    printf("ingrese segundo parametro: ");
+    scanf("%d",&b);
+
+
+    // suma
+    printf("la suma de los parametros es = %d\n ", a+b);
+
+    // resta
+    printf("la resta de los parametros es = %d\n ", a-b);
+    // multiplicacion
+    printf("la multi de los parametros es = %d\n ", a*b);
+    // division
+    printf("la division de los parametros es = %f\n ", a/b);
+    //potencia
+    printf("la potencia de los parametros es = %d\n ", a^b);
+    // residuo
+    printf("la el resiudo de los parametros es = %d\n ", a%b);
+    // inidique si es mayor igual o menor
+    
+
+
+
+    
+
+    
+}
